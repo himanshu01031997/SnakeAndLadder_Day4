46b64a638170c9896f5d11d01fdb36db07c881d7
himanshu01031997 <hs010397@gmail.com>
2022-10-24 22:46:28 +0530

    add:added initial Position

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            Console.WriteLine(snake.initialPosition);
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Snake
+    {
+        public int initialPosition = 0;
+
+    }
+}
